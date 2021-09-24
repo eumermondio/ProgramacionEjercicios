98291ffc6cc6cf737fb6b95293d4460257255ce0
Cristian <cpachecoarjona@gmail.com>
2021-09-24 10:29:12 +0200

    Ejercicio 1 - cap 1 -bloque 1

---
+++ src/capitulo01/bloque01/Ejercicio01.java
@@ -0,0 +1,20 @@
+package capitulo01.bloque01;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		String str = JOptionPane.showInputDialog("Introduzca un número entero: ");
+		int var1 = Integer.parseInt(str);
+		
+		str = JOptionPane.showInputDialog("Introduzca un número flotante: ");
+		float var2 = Float.parseFloat(str);
+		
+		str = JOptionPane.showInputDialog("Introduzca un número double: ");
+		double var3 = Double.parseDouble(str);
+		
+		System.out.println("Número introducido: " + var1 + " ,número flotante: " + var2 + " ,número double: " + var3);
+	}
+
+}
--- src/programacionEjercicios/miClase.java
+++
@@ -1,9 +0,0 @@
-package programacionEjercicios;
-
-public class miClase {
-
-	public static void main(String[] args) {
-System.out.println("Hola repositorio");
-	}
-
-}
