1847f25ce43d872a564f0e764f98cefe17df4f3e
Cristian <cpachecoarjona@gmail.com>
2021-10-01 10:15:58 +0200

    Primer ejercicio switch

---
+++ src/capitulo02/bloque03/Ejercicio01.java
@@ -0,0 +1,42 @@
+package capitulo02.bloque03;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+
+		String str = JOptionPane.showInputDialog("Dame la nota del alumno: ");
+		int nota = Integer.parseInt(str);
+		
+		switch (nota) {
+		case 0:
+		case 1:
+		case 2:
+			System.out.println("Muy deficiente");
+			break;
+		case 3:
+		case 4:
+			System.out.println("Insuficiente");
+			break;
+		case 5:
+			System.out.println("Suficiente");
+			break;
+		case 6:
+			System.out.println("Bien");
+			break;
+		case 7:
+		case 8:
+			System.out.println("Notable");
+			break;
+		case 9:
+		case 10:
+			System.out.println("Sobresaliente");
+			break;
+			default:			
+			System.out.println("La nota no es válida");
+		}
+		
+	}
+
+}
