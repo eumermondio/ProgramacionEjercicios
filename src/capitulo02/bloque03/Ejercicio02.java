3a217b966c9ac2f54afdbb1b81685ccedc90d53d
Cristian <cpachecoarjona@gmail.com>
2021-10-01 13:41:20 +0200

    Ejercicio02 a medio hacer

---
+++ src/capitulo02/bloque03/Ejercicio02.java
@@ -0,0 +1,42 @@
+package capitulo02.bloque03;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio02 {
+
+	public static void main(String[] args) {
+
+		System.out.println("--------Calculadora Avanzada-------");
+		System.out.println("0.-Salir");
+		System.out.println("1.-Raíces");
+		System.out.println("2.-Potencias");
+		System.out.println("3.-Módulo de la división");
+		
+		int x, y;
+
+		String str = JOptionPane.showInputDialog("Bienvenido a calculadora avanzada, Dime la opción deseada");
+		int opcion = Integer.parseInt(str);
+		
+		switch (opcion) {
+		case 0:
+			break;
+		case 1:
+			str = JOptionPane.showInputDialog("Modo raíces seleccionado\nDígame el radicando");
+			x = Integer.parseInt(str);
+			str = JOptionPane.showInputDialog("Dígame el índice");
+			y = Integer.parseInt(str);
+			System.out.println("El resultado es: " + Math.pow(x, 1.0f / y));
+			break;
+		case 2:
+			str = JOptionPane.showInputDialog("Dígame la base");
+			x = Integer.parseInt(str);
+			str = JOptionPane.showInputDialog("Dígame el exponente");
+			y = Integer.parseInt(str);
+			System.out.println("El resultado es: " + Math.pow(x, y));
+			break;
+		}
+		
+		
+	}
+
+}
