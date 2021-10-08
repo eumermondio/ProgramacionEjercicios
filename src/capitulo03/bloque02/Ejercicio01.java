48edb01b6d4583d2086188030171d646fc1eec36
Cristian <cpachecoarjona@gmail.com>
2021-10-08 13:20:06 +0200

    Ej 1 de la media hecho

--- src/capitulo03/bloque02/Ejercicio01.java
+++ src/capitulo03/bloque02/Ejercicio01.java
@@ -1,11 +1,23 @@
 package capitulo03.bloque02;
 
+import java.util.Scanner;
+
 public class Ejercicio01 {
 
 	public static void main(String[] args) {
 
-		
-		
+		Scanner sc = new Scanner(System.in);
+		int repes, num1, num2 = 1;
+		float acumulado = 0;
+		System.out.println("Dime de cuantos numeros quieres hacer la media");
+		repes = sc.nextInt();
+		for (int i = 0; i < repes; i++) {
+			System.out.println("Dime el número " + num2++);
+			num1 = sc.nextInt();
+			acumulado += num1;
+		}
+		System.out.println("La media es : " + acumulado / repes);
+
 	}
 
 }
---
+++ src/capitulo03/bloque02/Ejercicio02.java
@@ -0,0 +1,11 @@
+package capitulo03.bloque02;
+
+public class Ejercicio02 {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
