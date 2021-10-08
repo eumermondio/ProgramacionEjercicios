0c4f7e5b16c988bbf39384e9312358343d6eddc5
Cristian <cpachecoarjona@gmail.com>
2021-10-08 14:53:56 +0200

    Ejercicio02 mayor y menor hecho

--- src/capitulo03/bloque02/Ejercicio02.java
+++ src/capitulo03/bloque02/Ejercicio02.java
@@ -7,13 +7,22 @@ public class Ejercicio02 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int repes, num1;
+		int repes, num1, num2 = 1, mayor = 0, menor = 0;
 		System.out.println("Dime cuantos numeros quieres comparar");
 		repes = sc.nextInt();
 		for (int i = 0; i < repes; i++) {
+			System.out.println("Dime el numero " + num2++);
+			num1 = sc.nextInt();
+			if (num1 > mayor) {
+				mayor = num1;
+			}
+			if (num1 < mayor) {
+				menor = num1;
+			}
 			
 		}
-		
+		System.out.println("El número mayor es: " + mayor);
+		System.out.println("El número menor es: " + menor);
 	}
 
 }
