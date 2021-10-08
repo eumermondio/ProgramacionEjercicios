7e5236c609dc9dfe0dd7a4d7b4aea780b6136071
Cristian <cpachecoarjona@gmail.com>
2021-10-08 13:41:14 +0200

    Seguir

--- src/capitulo03/bloque02/Ejercicio02.java
+++ src/capitulo03/bloque02/Ejercicio02.java
@@ -1,10 +1,18 @@
 package capitulo03.bloque02;
 
+import java.util.Scanner;
+
 public class Ejercicio02 {
 
 	public static void main(String[] args) {
 
-		
+		Scanner sc = new Scanner(System.in);
+		int repes, num1;
+		System.out.println("Dime cuantos numeros quieres comparar");
+		repes = sc.nextInt();
+		for (int i = 0; i < repes; i++) {
+			
+		}
 		
 	}
 
