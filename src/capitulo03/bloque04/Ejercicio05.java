5557ec921682f0276766d8ddfe4622dbd229c19c
eumermondio <cpachecoarjona@gmail.com>
2021-10-15 19:55:06 +0200

    5 hecho

--- src/capitulo03/bloque04/Ejercicio05.java
+++ src/capitulo03/bloque04/Ejercicio05.java
@@ -1,10 +1,22 @@
 package capitulo03.bloque04;
 
+import java.util.Scanner;
+
 public class Ejercicio05 {
 
 	public static void main(String[] args) {
-		
-		
+
+		int num = 0, i = 1;
+		double factorial = 1;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("De qué número quieres saber el factorial?");
+		num = sc.nextInt();
+
+		while (i < num) {
+			factorial *= num;
+			num--;
+		}
+		System.out.println("El factorial es: " + factorial);
 
 	}
 
