87ac11a7e043dc3eb83f511523f75d0508e4a7e4
Cristian <cpachecoarjona@gmail.com>
2021-10-20 09:13:00 +0200

    De 15 a 150 puesto

--- src/capitulo05/bloque01/Ejercicio03.java
+++ src/capitulo05/bloque01/Ejercicio03.java
@@ -4,7 +4,7 @@ public class Ejercicio03 {
 
 	public static void main(String[] args) {
 
-		int array[] = new int[15];
+		int array[] = new int[150];
 		for (int i = 0; i < array.length; i++) {
 			array[i] = (int) Math.round(Math.random() * 100);
 			System.out.print(array[i] + " ");
