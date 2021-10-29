1d4ec648117be8cc600974fa27a1a09368f0c002
Cristian <cpachecoarjona@gmail.com>
2021-10-29 10:22:36 +0200

    Bien hechos

--- src/capitulo05/bloque03/Ejercicio03.java
+++ src/capitulo05/bloque03/Ejercicio03.java
@@ -4,7 +4,7 @@ public class Ejercicio03 {
 
 	public static void main(String[] args) {
 
-		int array[] = new int[20], aux = 0, i, salto = 0;
+		int array[] = new int[150], aux = 0, i, salto = 0;
 		for (i = 0; i < array.length; i++) { // Inicializar array
 			array[i] = (int) Math.round(Math.random() * 1000);
 			System.out.print(array[i] + " ");
