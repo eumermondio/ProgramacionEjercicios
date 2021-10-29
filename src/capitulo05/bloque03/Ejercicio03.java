68a3c06e4f0f42b3d41f3b15eea4549f97ff82de
Cristian <cpachecoarjona@gmail.com>
2021-10-29 10:14:20 +0200

    hechos, mirar shell para saber mejor como va

--- src/capitulo05/bloque03/Ejercicio03.java
+++ src/capitulo05/bloque03/Ejercicio03.java
@@ -4,15 +4,25 @@ public class Ejercicio03 {
 
 	public static void main(String[] args) {
 
-		int array[] = new int[150], aux = 0, i;
+		int array[] = new int[20], aux = 0, i, salto = 0;
 		for (i = 0; i < array.length; i++) { // Inicializar array
 			array[i] = (int) Math.round(Math.random() * 1000);
 			System.out.print(array[i] + " ");
 		}
 
 		System.out.println("");
-		
-		
+
+		for (salto = 6; salto > 0; salto--) { //Ordenar array SHELL
+			for (i = 0; i < array.length; i++) {
+				if ((i + salto) < array.length) {
+					if (array[i + salto] < array[i]) {
+						aux = array[i];
+						array[i] = array[i + salto];
+						array[i + salto] = aux;
+					}
+				}
+			}
+		}
 
 		for (i = 0; i < array.length; i++) { // Visualizar array ordenado
 			System.out.print(array[i] + " ");
