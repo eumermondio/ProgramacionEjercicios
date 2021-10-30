efd37ba2abfb1f01747e46896c24b578b20980fb
eumermondio <cpachecoarjona@gmail.com>
2021-10-30 11:20:11 +0200

    Tres en raya funcional 100%

--- src/capitulo05/bloque04opcional/Ejercicio07.java
+++ src/capitulo05/bloque04opcional/Ejercicio07.java
@@ -13,8 +13,8 @@ public class Ejercicio07 {
 		int filaAbajo[] = new int[3];
 
 		int opcion = 1, fila = 1, colu = 1;
-		boolean victoria1 = false;
-		boolean victoria2 = false;
+		boolean victoria = false;
+		// boolean victoria2 = false;
 
 		Scanner sc = new Scanner(System.in);
 
@@ -62,8 +62,8 @@ public class Ejercicio07 {
 
 			System.out.println("");
 
-			// DE AQUI EN ADELANTE EL SIGUIENTE FOR CONTEMPLA LAS 7 JUGADAS QUE PUEDE HABER
-			// EN UN JUEGO ( 6 POSICIONAMIENTOS MAXIMO )
+			// DE AQUI EN ADELANTE EL SIGUIENTE FOR CONTEMPLA LAS 9 JUGADAS QUE PUEDE HABER
+			// EN UN JUEGO ( 9 POSICIONAMIENTOS MAXIMO )
 			// DESPUES EN CASO DE QUE EL NUMERO DE MOVIMIENTO SEA IMPAR, JUGARÁ EL 1, EN
 			// CASO DE QUE SEA PAR JUGARÁ EL 2, HE USADO LAS
 			// POSIBILIDADES DE JUGADAS COMO DISCRIMINANTE PARA SABER SI ESTARÁ JUGANDO 1 O
@@ -77,9 +77,9 @@ public class Ejercicio07 {
 			// 1 O 2 DEPENDIENDO DEL JUGADOR, EN CASO DE
 			// QUE ESA CASILLA YA ESTE OCUPADA NO HARA NADA
 
-			for (int movs = 7; movs > 0; movs--) {
+			for (int movs = 9; movs > 0; movs--) {
 				if ((movs & 1) == 1) {
-					while (!victoria1) {
+					while (!victoria) {
 						System.out.println("Jugador 1");
 						System.out.println("Elige fila");
 						fila = sc.nextInt();
@@ -88,10 +88,11 @@ public class Ejercicio07 {
 						if ((fila < 0 || fila > 3) || (colu < 0 || colu > 2)) {
 							return;
 						}
-						
-						// RECORRER FILA DE ARRIBA Y EN CASO DE QUE LA FILA SEA 1, ES DECIR ESA FILA ES LA SELECCIONADA,
+
+						// RECORRER FILA DE ARRIBA Y EN CASO DE QUE LA FILA SEA 1, ES DECIR ESA FILA ES
+						// LA SELECCIONADA,
 						// Y NO ESTA OCUPADA INSERTARÁ UN 1 A LA COLUMNA, ES DECIR, EL INDICE DEL ARRAY
-						
+
 						for (int i = 0; i < filaArriba.length; i++) {
 
 							if (fila == 1 && (filaArriba[colu] != 1) && (filaArriba[colu] != 2)) {
@@ -100,7 +101,7 @@ public class Ejercicio07 {
 							}
 
 						}
-						
+
 						// MOSTRAR FILA EN PANTALLA
 
 						for (int i = 0; i < filaArriba.length; i++) {
@@ -108,8 +109,9 @@ public class Ejercicio07 {
 						}
 
 						System.out.println("");
-						
-						// RECORRER FILA DE MEDIO Y EN CASO DE QUE LA FILA SEA 2, ES DECIR ESA FILA ES LA SELECCIONADA,
+
+						// RECORRER FILA DE MEDIO Y EN CASO DE QUE LA FILA SEA 2, ES DECIR ESA FILA ES
+						// LA SELECCIONADA,
 						// Y NO ESTA OCUPADA INSERTARÁ UN 1 A LA COLUMNA, ES DECIR, EL INDICE DEL ARRAY
 
 						for (int i = 0; i < filaMedio.length; i++) {
@@ -119,7 +121,7 @@ public class Ejercicio07 {
 							}
 
 						}
-						
+
 						// MOSTRAR FILA EN PANTALLA
 
 						for (int i = 0; i < filaMedio.length; i++) {
@@ -128,9 +130,10 @@ public class Ejercicio07 {
 
 						System.out.println("");
 
-						// RECORRER FILA DE ABAJO Y EN CASO DE QUE LA FILA SEA 3, ES DECIR ESA FILA ES LA SELECCIONADA,
+						// RECORRER FILA DE ABAJO Y EN CASO DE QUE LA FILA SEA 3, ES DECIR ESA FILA ES
+						// LA SELECCIONADA,
 						// Y NO ESTA OCUPADA INSERTARÁ UN 1 A LA COLUMNA, ES DECIR, EL INDICE DEL ARRAY
-						
+
 						for (int i = 0; i < filaAbajo.length; i++) {
 
 							if (fila == 3 && (filaAbajo[colu] != 1) && (filaAbajo[colu] != 2)) {
@@ -140,24 +143,73 @@ public class Ejercicio07 {
 							}
 
 						}
-						
+
 						// MOSTRAR FILA EN PANTALLA
-						
+
 						for (int i = 0; i < filaAbajo.length; i++) {
 							System.out.print(filaAbajo[i] + " ");
 						}
 
 						System.out.println("");
 
+						// COMPRUEBO SI EL JUGADOR 1 HA GANADO HACIENDO LA COMPARATIVA DE LAS POSICIONES
+						// DEL
+						// ARRAY ( FILA DE ARRIBA )
+						// YA QUE SI EL JUGADOR HA PUESTO LAS 3 POSIS SUYAS ( 1 ) EN FILA DE ARRIBA
+						// SEGUIDAS ESTO DARÁ SU VICTORIA
+						// POR LO QUE SE REFLEJA LA VICTORIA DEL JUGADOR 1 EN ESTE CASO, LO MISMO SE
+						// APLICARÁ AL JUGADOR 2
+						// ESTAS CONDICIONES SON SI LAS CASILLAS HORIZONTALES SON IGUALES
+
+						if (filaArriba[0] == 1 && filaArriba[1] == 1 && filaArriba[2] == 1) {
+							victoria = true;
+							System.out.println("Enhorabuena Jugador 1!");
+						}
+						if (filaMedio[0] == 1 && filaMedio[1] == 1 && filaMedio[2] == 1) {
+							victoria = true;
+							System.out.println("Enhorabuena Jugador 1!");
+						}
+						if (filaAbajo[0] == 1 && filaAbajo[1] == 1 && filaAbajo[2] == 1) {
+							victoria = true;
+							System.out.println("Enhorabuena Jugador 1!");
+						}
+
+						// AHORA COMPROBARE LAS COLUMNAS
+
+						if (filaArriba[0] == 1 && filaMedio[0] == 1 && filaAbajo[0] == 1) {
+							victoria = true;
+							System.out.println("Enhorabuena Jugador 1!");
+						}
+						if (filaArriba[1] == 1 && filaMedio[1] == 1 && filaAbajo[1] == 1) {
+							victoria = true;
+							System.out.println("Enhorabuena Jugador 1!");
+						}
+						if (filaArriba[2] == 1 && filaMedio[2] == 1 && filaAbajo[2] == 1) {
+							victoria = true;
+							System.out.println("Enhorabuena Jugador 1!");
+						}
+
+						// AHORA COMPRUEBO LAS DIAGONALES
+
+						if (filaArriba[0] == 1 && filaMedio[1] == 1 && filaAbajo[2] == 1) {
+							victoria = true;
+							System.out.println("Enhorabuena Jugador 1!");
+						}
+						if (filaArriba[2] == 1 && filaMedio[1] == 1 && filaAbajo[0] == 1) {
+							victoria = true;
+							System.out.println("Enhorabuena Jugador 1!");
+						}
+
 						break;
 
 					}
 				}
-				
-				// PARTE DEL JUGADOR 2, LO MISMO QUE PARA EL JUGADOR 1 PERO EN VEZ DE METER EL VALOR 1 AL ARRAY METO EL 2
+
+				// PARTE DEL JUGADOR 2, LO MISMO QUE PARA EL JUGADOR 1 PERO EN VEZ DE METER EL
+				// VALOR 1 AL ARRAY METO EL 2
 
 				if ((movs & 1) == 0) {
-					while (!victoria2) {
+					while (!victoria) {
 						System.out.println("Jugador 2");
 						System.out.println("Elige fila");
 						fila = sc.nextInt();
@@ -213,10 +265,53 @@ public class Ejercicio07 {
 
 						System.out.println("");
 
+						if (filaArriba[0] == 2 && filaArriba[1] == 2 && filaArriba[2] == 2) {
+							victoria = true;
+							System.out.println("Enhorabuena Jugador 2!");
+						}
+						if (filaMedio[0] == 2 && filaMedio[1] == 2 && filaMedio[2] == 2) {
+							victoria = true;
+							System.out.println("Enhorabuena Jugador 2!");
+						}
+						if (filaAbajo[0] == 2 && filaAbajo[1] == 2 && filaAbajo[2] == 2) {
+							victoria = true;
+							System.out.println("Enhorabuena Jugador 2!");
+						}
+
+						// AHORA COMPROBARE LAS COLUMNAS
+
+						if (filaArriba[0] == 2 && filaMedio[0] == 2 && filaAbajo[0] == 2) {
+							victoria = true;
+							System.out.println("Enhorabuena Jugador 2!");
+						}
+						if (filaArriba[1] == 2 && filaMedio[1] == 2 && filaAbajo[1] == 2) {
+							victoria = true;
+							System.out.println("Enhorabuena Jugador 2!");
+						}
+						if (filaArriba[2] == 2 && filaMedio[2] == 2 && filaAbajo[2] == 2) {
+							victoria = true;
+							System.out.println("Enhorabuena Jugador 2!");
+						}
+
+						// AHORA COMPRUEBO LAS DIAGONALES
+
+						if (filaArriba[0] == 2 && filaMedio[1] == 2 && filaAbajo[2] == 2) {
+							victoria = true;
+							System.out.println("Enhorabuena Jugador 2!");
+						}
+						if (filaArriba[2] == 2 && filaMedio[1] == 2 && filaAbajo[0] == 2) {
+							victoria = true;
+							System.out.println("Enhorabuena Jugador 2!");
+						}
+
 						break;
 
 					}
 				}
+
+			}
+			if (!victoria) {
+				System.out.println("Empate");
 
 			}
 		}
@@ -224,6 +319,3 @@ public class Ejercicio07 {
 	}
 
 }
-
-
-// HACER CONDICION DE VICTORIAS
