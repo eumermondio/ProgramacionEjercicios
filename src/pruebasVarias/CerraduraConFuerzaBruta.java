c0d724fbfb54f8e14b4a8ef5e24b96b5680dcdb3
eumermondio <cpachecoarjona@gmail.com>
2021-11-08 19:39:25 +0100

    Algoritmo de fuerza bruta muy bien optimizado

--- src/pruebasVarias/CerraduraConFuerzaBruta.java
+++ src/pruebasVarias/CerraduraConFuerzaBruta.java
@@ -6,34 +6,32 @@ public class CerraduraConFuerzaBruta {
 
 	public static void main(String[] args) {
 
-		int cerradura[] = new int[6];
-		int llave[] = new int[6];
+		int cerradura[] = new int[700000];
+		int llave[] = new int[cerradura.length];
 		int cont = 0;
-		int indiceExcluido[] = new int[llave.length];
 		boolean estaAbierto = false;
 		System.out.println("Cerradura");
 		for (int i = 0; i < cerradura.length; i++) {
-			cerradura[i] = (int) Math.round(Math.random() * 5);
+			cerradura[i] = (int) Math.round(Math.random() * 100);
 
 		}
 		long millisDesde1970 = new Date().getTime();
 		do {
 
 			for (int i = 0; i < llave.length; i++) {
-				llave[i] = (int) Math.round(Math.random() * 5);
+				llave[i] = (int) Math.round(Math.random() * 100);
+				if (llave[i] == cerradura[i]) {
+					cont++;
+				} else {
+					i--;
+				}
 			}
 			System.out.println("Llave");
 			for (int i = 0; i < llave.length; i++) {
 				System.out.print(llave[i] + " ");
 			}
 			System.out.println("");
-			for (int i = 0; i < llave.length; i++) {
-				if (llave[i] == cerradura[i]) {
-					cont++;
-					indiceExcluido[i] = i;
-				}
 
-			}
 			if (cont == cerradura.length) {
 				estaAbierto = true;
 			}
@@ -52,8 +50,5 @@ public class CerraduraConFuerzaBruta {
 		long restaDeMillis = millisDespuesDeBusquedaPrimos - millisDesde1970;
 
 		System.out.println("He tardado " + restaDeMillis + " milisegundos - " + (restaDeMillis / 1000) + " segundos");
-		for (int i = 0; i < indiceExcluido.length; i++) {
-			System.out.print(indiceExcluido[i] + " ");
-		}
 	}
 }
