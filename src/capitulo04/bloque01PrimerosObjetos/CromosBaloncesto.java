1c20f2976abbe3bb54b2c59a027dc91cc13a2c9f
Cristian <cpachecoarjona@gmail.com>
2021-11-25 13:22:12 +0100

    Primeros objetos

---
+++ src/capitulo04/bloque01PrimerosObjetos/CromosBaloncesto.java
@@ -0,0 +1,57 @@
+package capitulo04.bloque01PrimerosObjetos;
+
+public class CromosBaloncesto {
+	private String nombreJugador;
+	private int altura;
+	private int peso;
+	private float puntosMedios;
+
+	public CromosBaloncesto() {
+
+	}
+
+	public CromosBaloncesto(String nombreJugador, int altura, int peso, float puntos) {
+		this.nombreJugador = nombreJugador;
+		this.altura = altura;
+		this.peso = peso;
+		this.puntosMedios = puntos;
+	}
+
+	public String toString() {
+		return "cromosBaloncesto [nombreJugador=" + nombreJugador + ", altura=" + altura + ", peso=" + peso
+				+ ", puntosMedios=" + puntosMedios + "]";
+	}
+
+	public String getNombreJugador() {
+		return nombreJugador;
+	}
+
+	public void setNombreJugador(String nombreJugador) {
+		this.nombreJugador = nombreJugador;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	public float getPuntos() {
+		return puntosMedios;
+	}
+
+	public void setPuntosMedios(float puntosMedios) {
+		this.puntosMedios = puntosMedios;
+	}
+
+}
---
+++ src/capitulo04/bloque01PrimerosObjetos/GestionaCromos.java
@@ -0,0 +1,21 @@
+package capitulo04.bloque01PrimerosObjetos;
+
+public class GestionaCromos {
+
+	public static void main(String[] args) {
+
+		CromosBaloncesto cromo1 = new CromosBaloncesto("Jordan", 194, 98, 37);
+		CromosBaloncesto cromo2 = new CromosBaloncesto("Kobe Bryant", 198, 96, 81);
+		CromosBaloncesto cromo3 = new CromosBaloncesto("Stephen Curry", 188, 86, 50);
+		CromosBaloncesto cromo4 = new CromosBaloncesto("Muresan", 231, 147, 31);
+		CromosBaloncesto cromo5 = new CromosBaloncesto("Paul Gasol", 215, 113, 45);
+
+		System.out.println(cromo1.toString());
+		System.out.println(cromo2.toString());
+		System.out.println(cromo3.toString());
+		System.out.println(cromo4.toString());
+		System.out.println(cromo5.toString());
+
+	}
+
+}
