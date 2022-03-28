9433eea8ab27173bc14cd1bfbc00c4bdffa80a1e
Cristian <cpachecoarjona@gmail.com>
2022-03-28 12:57:05 +0200

    hacer la ventana de BBDD

---
+++ src/capitulo08/Ejercicio01/Kk.java
@@ -0,0 +1,80 @@
+package capitulo08.Ejercicio01;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JTextField;
+import java.awt.Insets;
+
+public class Kk {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JLabel lblGestionDeFabricantes;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Kk window = new Kk();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Kk() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] { 0, 0, 0 };
+		gridBagLayout.rowHeights = new int[] { 0, 0, 0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
+		frame.getContentPane().setLayout(gridBagLayout);
+		
+		lblGestionDeFabricantes = new JLabel("GESTION DE FABRICANTES");
+		GridBagConstraints gbc_lblGestionDeFabricantes = new GridBagConstraints();
+		gbc_lblGestionDeFabricantes.gridwidth = 2;
+		gbc_lblGestionDeFabricantes.insets = new Insets(0, 0, 5, 5);
+		gbc_lblGestionDeFabricantes.gridx = 0;
+		gbc_lblGestionDeFabricantes.gridy = 0;
+		frame.getContentPane().add(lblGestionDeFabricantes, gbc_lblGestionDeFabricantes);
+
+		JLabel lblId = new JLabel("Id:");
+		GridBagConstraints gbc_lblId = new GridBagConstraints();
+		gbc_lblId.insets = new Insets(0, 0, 0, 5);
+		gbc_lblId.anchor = GridBagConstraints.EAST;
+		gbc_lblId.gridx = 0;
+		gbc_lblId.gridy = 1;
+		frame.getContentPane().add(lblId, gbc_lblId);
+
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 1;
+		frame.getContentPane().add(textField, gbc_textField);
+		textField.setColumns(10);
+	}
+
+}
