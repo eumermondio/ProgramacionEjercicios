047bfacdef549b08dd9cedd9c86226618b032756
eumermondio <cpachecoarjona@gmail.com>
2022-04-06 19:25:35 +0200

    pruebas

---
+++ src/kk/PruebaComponentes.java
@@ -0,0 +1,117 @@
+package kk;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JSlider;
+import java.awt.Insets;
+import javax.swing.SwingConstants;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.JTextArea;
+import javax.swing.JProgressBar;
+
+public class PruebaComponentes {
+
+	private JFrame frame;
+	private JSlider slider;
+	private JLabel lblNewLabel;
+	private JTextArea textArea;
+	private JProgressBar progressBar;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PruebaComponentes window = new PruebaComponentes();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PruebaComponentes() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		frame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
+		JPanel panel = new JPanel();
+		tabbedPane.add("Prueba 1", panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[] { 0, 0, 0 };
+		gbl_panel.rowHeights = new int[] { 0, 0, 0 };
+		gbl_panel.columnWeights = new double[] { 0.0, 0.0, 1.0 };
+		gbl_panel.rowWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
+		panel.setLayout(gbl_panel);
+
+		lblNewLabel = new JLabel("Pruebas");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		panel.add(lblNewLabel, gbc_lblNewLabel);
+
+		slider = new JSlider();
+		slider.setMaximum(10);
+		slider.setMinorTickSpacing(1);
+		slider.setPaintLabels(true);
+		slider.setPaintTicks(true);
+		slider.setValue(0);
+		slider.setMajorTickSpacing(1);
+		GridBagConstraints gbc_slider = new GridBagConstraints();
+		gbc_slider.insets = new Insets(0, 0, 5, 0);
+		gbc_slider.gridwidth = 2;
+		gbc_slider.gridx = 1;
+		gbc_slider.gridy = 0;
+		panel.add(slider, gbc_slider);
+
+		textArea = new JTextArea();
+		GridBagConstraints gbc_textArea = new GridBagConstraints();
+		gbc_textArea.insets = new Insets(0, 0, 0, 5);
+		gbc_textArea.fill = GridBagConstraints.BOTH;
+		gbc_textArea.gridx = 0;
+		gbc_textArea.gridy = 1;
+		panel.add(textArea, gbc_textArea);
+
+		progressBar = new JProgressBar();
+		progressBar.setStringPainted(true);
+		GridBagConstraints gbc_progressBar = new GridBagConstraints();
+		gbc_progressBar.gridwidth = 2;
+		gbc_progressBar.insets = new Insets(0, 0, 0, 5);
+		gbc_progressBar.gridx = 1;
+		gbc_progressBar.gridy = 1;
+		panel.add(progressBar, gbc_progressBar);
+		slider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				textArea.setText("");
+				textArea.append("" + slider.getValue());
+				progressBar.setString("" + slider.getValue() * 10 + " %");
+			}
+		});
+		tabbedPane.add("Prueba 2", new JPanel());
+	}
+
+}
